1efa1a684c757783b5ed3abf78ccf8ea92ce8e28
guidote <74155514+guidote@users.noreply.github.com>
2021-02-17 22:23:40 -0500

    Delete VegetablesAisle directory

--- VegetablesAisle/Img/images sources.bat
+++
@@ -1,9 +0,0 @@
-BROCOLI: https://www.pexels.com/photo/green-broccoli-vegetable-on-brown-wooden-table-47347/
-
-CARROTS: https://www.pexels.com/photo/8-piece-of-carrot-on-brown-chopping-board-65174/
-
-EGGPLANT: 
-
-TOMATO: https://www.pexels.com/photo/red-tomato-5617/
-
-ZUCHINNI: https://www.pexels.com/photo/3-zuchini-brown-wooden-surface-128420/
